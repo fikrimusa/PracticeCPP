14b72204f30a84ecdd81d1f2e02c7ed3f29abd15
Fikri Musa <nurfikrimusa@gmail.com>
2024-07-26 22:10:38 +0800

    Updated codes

--- TheBasics/StatementAndExpressions/SE.cpp
+++ TheBasics/StatementAndExpressions/SE.cpp
@@ -1,16 +1,16 @@
 // C++20
-// #include <format>
-// #include <iostream>
+#include <format>
+#include <iostream>
 
-// using std::cout;
-// using std::format;
+using std::cout;
+using std::format;
 
-// int main() {
-//     int x;
-//     x = 42;
+int main() {
+    int x;
+    x = 42;
 
-//     cout << x;
+    cout << x;
 
-//     auto str = format("x is {}\n", x);
-//     cout << str;
-// }
+    auto str = format("x is {}\n", x);
+    cout << str;
+}
