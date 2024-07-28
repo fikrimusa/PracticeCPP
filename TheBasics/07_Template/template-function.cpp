514706ee7395ba2c14b07e492be288930d259e79
Fikri Musa <nurfikrimusa@gmail.com>
2024-07-29 01:20:33 +0800

    Add new example

---
+++ TheBasics/07_Template/template-function.cpp
@@ -0,0 +1,18 @@
+#include <format>
+#include <iostream>
+
+using std::format;
+using std::cout;
+
+template<typename T>
+T maxof(T a, T b) {
+    return a > b ? a : b;
+}
+
+int main() {
+    int x {47};
+    int y {73};
+    auto z = maxof<int>(x, y);
+    cout << format("max is {}\n", z);
+    return 0;
+}
